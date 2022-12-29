fd183110c2b5fceb3d199526f57c33026ca13fca
Abdelhamid salhi <essalhi12345@gmail.com>
2022-12-29 18:07:59 +0100

    finished design for the for form for registration.

--- app/src/main/java/com/example/intent1/ConnectManager.java
+++ app/src/main/java/com/example/intent1/ConnectManager.java
@@ -1,14 +1,19 @@
 package com.example.intent1;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 public class ConnectManager extends AppCompatActivity {
     EditText username;
     EditText password;
     Button loginButton;
+    TextView signUpView;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,15 +22,20 @@ public class ConnectManager extends AppCompatActivity {
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
-                    Toast.makeText(ConnectManager.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(ConnectManager.this, "Login Failed!", Toast.LENGTH_SHORT).show();
-                }
+        signUpView = findViewById(R.id.signupText);
+
+        signUpView.setOnClickListener(view -> {
+            intent = new Intent(ConnectManager.this, RegistrationActivity.class);
+            startActivity(intent);
+        });
+
+        loginButton.setOnClickListener(view -> {
+            if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                Toast.makeText(ConnectManager.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(ConnectManager.this, "Login Failed!", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
